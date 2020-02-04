66318e7c827d35d714b164f1dffb4d411d24d51f
Shruthi Kumar <sk593@duke.edu>
2020-02-04 00:29:19 -0500

    added comments to all cell classes

--- src/cellsociety/Model/FireCell.java
+++ src/cellsociety/Model/FireCell.java
@@ -5,6 +5,17 @@ import java.awt.Point;
 import java.util.HashMap;
 import java.util.Random;
 
+/**
+ * Fire Cell class based on Fire simulation. Users can choose the fire simulation and then cells are created
+ * based on this type of simulation. Thus, FireCell is an subclass
+ * Purpose: This creates the fire cells that will populate the grid. This class is now a subclass. We made this design decision because
+ *  not all simulations have the same rules. Having extended cells classes will allow the fire cell to have its own rule set while
+ *  still implementing basic cell functions.
+ * Assumptions: The class will work assuming all dependencies are functioning.
+ * Dependencies: This class relies on the Grid class to instantiate it correctly and the Cell class to properly override its methods
+ * Example: Choose a simulation and then the program will correctly instatiate the fire cells.
+ * @author Shruthi Kumar, Chris Warren, Aneesh Gupta
+ */
 public class FireCell extends Cell {
 
   private float probCatch;
@@ -15,6 +26,13 @@ public class FireCell extends Cell {
   private int state;
   private Random numChooser = new Random();
 
+  /**
+   * Constructor for the FireCell object
+   * @param row: row number cell is in
+   * @param col: column number cell is in
+   * @param mystate: current state of the cell
+   * @param prob : probability of catching fire
+   */
   public FireCell(int row, int col, int mystate, float prob) {
     super(row, col, mystate);
     this.state = mystate;
@@ -23,6 +41,15 @@ public class FireCell extends Cell {
   }
 
 
+  /**
+   * Updates the cell based on the rules
+   * @param  cellHashMap: grid of cells
+   * @param  row: row the cell is in
+   * @param  col: column the cell is in
+   * @param  width: width of the grid
+   * @param  height : height of the grid
+   * @return int : the next state integer
+   */
   @Override
   public int updateCell(HashMap<Point, Cell> cellHashMap, int row, int col, int width, int height) {
     if(checkState(cellHashMap, row, col, BURNING)) {
@@ -37,19 +64,46 @@ public class FireCell extends Cell {
     return state;
   }
 
-  private boolean checkState(HashMap<Point, Cell> cellHashMap, int row, int col, int currState) {
-    return cellHashMap.get(new Point(row, col)).getState() == currState;
-  }
-
-
+  /**
+   * Returns the state of the cell
+   * @return state of the cell
+   */
   @Override
   public int getState() {
     return this.state;
   }
 
+  /**
+   * Returns the color of the cell
+   * @return color of the cell
+   */
   @Override
   public Color getCellColor() {
     return cellColor;
+  }
+
+  /**
+   * Sets the color of the cell
+   */
+  @Override
+  public void setCellColor() {
+    if(state == EMPTY) {
+      cellColor = Color.YELLOW;
+    }
+    else if(state == TREE) {
+      cellColor = Color.GREEN;
+    }
+    else {
+      cellColor = Color.RED;
+    }
+  }
+
+  /**
+   * Sets the color of the cell
+   * @param state : state of the cell
+   */
+  public void setState(int state) {
+    this.state = state;
   }
 
 
@@ -72,21 +126,8 @@ public class FireCell extends Cell {
     return cellHashMap.containsKey(new Point(rowDelta, colDelta));
   }
 
-  @Override
-  public void setCellColor() {
-    if(state == EMPTY) {
-      cellColor = Color.YELLOW;
-    }
-    else if(state == TREE) {
-      cellColor = Color.GREEN;
-    }
-    else {
-      cellColor = Color.RED;
-    }
-  }
-
-  public void setState(int state) {
-    this.state = state;
+  private boolean checkState(HashMap<Point, Cell> cellHashMap, int row, int col, int currState) {
+    return cellHashMap.get(new Point(row, col)).getState() == currState;
   }
 
 }
--- src/cellsociety/Model/SegregationCell.java
+++ src/cellsociety/Model/SegregationCell.java
@@ -7,6 +7,17 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * SegregationCell class based on Segregation simulation. Users can choose the segregation simulation and then cells are created
+ * based on this type of simulation. Thus, SegregationCell is an subclass
+ * Purpose: This creates the segregation cells that will populate the grid. This class is now a subclass. We made this design decision because
+ *  not all simulations have the same rules. Having extended cells classes will allow the segregation cell to have its own rule set while
+ *  still implementing basic cell functions.
+ * Assumptions: The class will work assuming all dependencies are functioning.
+ * Dependencies: This class relies on the Grid class to instantiate it correctly and the Cell class to properly override its methods
+ * Example: Choose a simulation and then the program will correctly instantiate the segregation cells.
+ * @author Shruthi Kumar, Chris Warren, Aneesh Gupta
+ */
 public class SegregationCell extends Cell {
 
   private int state;
@@ -18,6 +29,13 @@ public class SegregationCell extends Cell {
   private Color cellColor;
   private List<Point> vacantCells;
 
+  /**
+   * Constructor for the FireCell object
+   * @param width: width of grid
+   * @param height: height of grid
+   * @param state: current state of the cell
+   * @param thresh : threshold for moving groups
+   */
   public SegregationCell(int width, int height, int state, double thresh) {
     super(width, height, state);
     this.state = state;
@@ -26,6 +44,15 @@ public class SegregationCell extends Cell {
     this.setCellColor();
   }
 
+  /**
+   * Updates the cell based on the rules
+   * @param  cellHashMap: grid of cells
+   * @param  row: row the cell is in
+   * @param  col: column the cell is in
+   * @param  width: width of the grid
+   * @param  height : height of the grid
+   * @return int : the next state integer
+   */
   @Override
   public int updateCell(HashMap<Point, Cell> cellHashMap, int row, int col, int width, int height) {
     getVacantCells(cellHashMap, width, height);
@@ -44,32 +71,55 @@ public class SegregationCell extends Cell {
     return myNextState;
   }
 
+  /**
+   * Returns the state of the cell
+   * @return state of the cell
+   */
   @Override
   public int getState() {
     return this.state;
   }
+
+  /**
+   * Sets the color of the cell
+   * @param myState : state of the cell
+   */
   @Override
   public void setState(int myState){
     state = myNextState;
     myNextState = VACANT;
   }
 
+  /**
+   * Sets the color of the cell
+   * @param nextState : next state of the cell
+   */
   @Override
   public void setMyNextState(int nextState){
     this.myNextState = nextState;
   }
 
+  /**
+   * Returns the next state of the cell
+   * @return next state of the cell
+   */
   @Override
   public int getNextState(){
     return this.myNextState;
   }
 
+  /**
+   * Returns the color of the cell
+   * @return color of the cell
+   */
   @Override
   public javafx.scene.paint.Color getCellColor() {
     return cellColor;
   }
 
-
+  /**
+   * Sets the color of the cell
+   */
   @Override
   public void setCellColor() {
     if(state == 0) {
@@ -83,7 +133,7 @@ public class SegregationCell extends Cell {
     }
   }
 
-  public void getVacantCells(HashMap<Point, Cell> cellHashMap, int width, int height) {
+  private void getVacantCells(HashMap<Point, Cell> cellHashMap, int width, int height) {
     vacantCells  = new ArrayList<>();
     for(int i = 0; i < width; i++) {
       for(int k = 0; k < height; k++) {
@@ -103,7 +153,7 @@ public class SegregationCell extends Cell {
     return cellHashMap.get(new Point(i, k)).getState() == vacant;
   }
 
-  public int getNeighborCount(HashMap<Point, Cell> cellHashMap, int row, int col) {
+  private int getNeighborCount(HashMap<Point, Cell> cellHashMap, int row, int col) {
     int count = 0;
     int delta = 1;
 
@@ -128,9 +178,8 @@ public class SegregationCell extends Cell {
     return cellHashMap.get(new Point(row, col)).getState() != VACANT;
   }
 
-  public int getNeighborTypeCount(HashMap<Point, Cell> cellHashMap, int row, int col, int state) {
+  private int getNeighborTypeCount(HashMap<Point, Cell> cellHashMap, int row, int col, int state) {
     int count = 0;
-    int delta = 1;
 
     int[] rowDelta = {-1, -1, -1, 0, 0, 1, 1, 1};
     int[] colDelta = {-1, 0, 1, -1, 1, -1, 0, 1};
