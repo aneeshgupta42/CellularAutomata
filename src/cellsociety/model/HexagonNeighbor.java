b56678c95a9c025e2243186d56e773b9d62a787e
Shruthi Kumar <sk593@duke.edu>
2020-02-07 02:54:02 -0500

    resolving merge conflict

--- src/cellsociety/model/HexagonNeighbor.java
+++ src/cellsociety/model/HexagonNeighbor.java
@@ -1,19 +1,25 @@
 package cellsociety.model;
 
 public class HexagonNeighbor extends Neighbor {
+  private int[] rowDelta;
+  private int[] colDelta;
 
   @Override
   public void updateRowColDeltas(int[] rowDeltaNew, int[] colDeltaNew) {
-
+    this.rowDelta = rowDeltaNew;
+    this.colDelta = colDeltaNew;
   }
 
   @Override
   public void setDirectNeighbors() {
-
+    setAllNeighbors();
   }
 
   @Override
   public void setAllNeighbors() {
-
+    rowDelta = new int[]{-1, 0, 1, 1, 0, -1};
+    colDelta = new int[]{-1, -1, -1, 0, 1, 0};
+    setRowDelta(rowDelta);
+    setColDelta(colDelta);
   }
 }
