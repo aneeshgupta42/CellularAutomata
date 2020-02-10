558a19216d42cfd01b1ab074816261c76a854213
Aneesh Gupta <ag468@duke.edu>
2020-02-09 21:51:16 -0500

    Shifted main running file to cellsociety/Main.java instead of inside view. Working on readme

---
+++ src/cellsociety/Main.java
@@ -0,0 +1,10 @@
+package cellsociety;
+
+import cellsociety.view.Display;
+
+public class Main {
+    public static void main(String[] args) {
+        Display start = new Display();
+        start.display(args);
+    }
+}
